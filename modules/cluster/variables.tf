e26398e5ce69c094eb8b268410c5c8f0ff15a945
Yizhak <yizhako@checkpoint.com>
2026-06-28 15:21:38 +0000

    feat(CGNSPC-3144): Update R82 GW BYOL default images in modules

--- modules/cluster/variables.tf
+++ modules/cluster/variables.tf
@@ -19,7 +19,7 @@ variable "prefix" {
 variable "source_image" {
   description = "The image name for the disk for the VM instance."
   type        = string
-  default     = "projects/checkpoint-public/global/images/check-point-r82-gw-byol-cluster-777-991002151-v20260618"
+  default     = "projects/checkpoint-public/global/images/check-point-r82-gw-byol-cluster-779-991002161-v20260628"
 }
 
 variable "os_version" {
--- modules/single/variables.tf
+++ modules/single/variables.tf
@@ -20,7 +20,7 @@ variable "prefix" {
 variable "source_image" {
   type        = string
   description = "The image name for the disk for the VM instance."
-  default     = "projects/checkpoint-public/global/images/check-point-r82-gw-byol-single-777-991002151-v20260618"
+  default     = "projects/checkpoint-public/global/images/check-point-r82-gw-byol-single-779-991002161-v20260628"
 }
 
 variable "os_version" {
